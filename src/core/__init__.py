b81eff5aa97e8a9290d368fdd8856053861810a6
Vadim <94108043+Showtimeeee@users.noreply.github.com>
2026-05-01 00:53:10 +0300

    Update __init__.py

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -2,4 +2,4 @@ from .rate_limiter import RateLimiter
 from .validator import MessageValidator
 from .message_queue import MessageQueue
 
-__all__ = ["RateLimiter", "MessageValidator", "MessageQueue"]
+__all__ = ["RateLimiter", "MessageValidator", "MessageQueue", "Database"]
